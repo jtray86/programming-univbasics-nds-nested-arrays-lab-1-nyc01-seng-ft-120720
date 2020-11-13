55a985b017d47d8f4ca34bac9f7c60739f3c4879
Jen Tracy <jenniferetracy@gmail.com>
2020-11-13 21:31:31 +0000

    Automatically backed up by Learn

--- lib/nested_array.rb
+++ lib/nested_array.rb
@@ -65,7 +65,7 @@ def matrix_lookup(matrix, row, column)
   # Given any matrix (array of arrays), a row index and a column index, 
   # Return the matrix's content at that row and and column
  matrix
-    storage_room[row][column]
+    matrix[row][column]
    
 end
 
