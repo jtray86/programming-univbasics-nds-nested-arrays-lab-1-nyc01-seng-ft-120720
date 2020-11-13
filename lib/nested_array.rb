bd46d54dc2a67e5b78ac43e6a9366287ac392698
Jen Tracy <jenniferetracy@gmail.com>
2020-11-13 20:53:40 +0000

    Automatically backed up by Learn

--- lib/nested_array.rb
+++ lib/nested_array.rb
@@ -52,8 +52,10 @@ def sorted_matrix
   "Asparagus"]
     ]
   
-    storage_room[0].sort!
-    storage_room[1].sort!
+    storage_room[0].sort
+    storage_room[1].sort
+    
+    storage_room
 end
 
 def matrix_lookup(matrix, row, column)
